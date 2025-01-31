ea4e69abf9a3d42138a3d7222ffec583f558884e
Ruslan <ruslan.minatullaev-1000@yandex.ru>
2025-01-31 17:45:42 +0300

    Fix bags/Refactoring

--- common.blocks/theme-toggle/theme-toggle.js
+++ common.blocks/theme-toggle/theme-toggle.js
@@ -1,17 +1,19 @@
-class dark{
+class dark {
   static textColor = "white";
   static backgroundColor = "hsla(0, 0%, 5%, 0.3)";
   static windowBackgroundColor = "hsl(0, 0%, 5%)";
   static buttonBackgroundColor = "hsla(0, 0%, 15%, 0.5)";
-  static backgroundImage = 'url("images/backgrounds/dark/mesh-294.avif")';
+  static backgroundImage = 'url("./images/backgrounds/dark/mesh-294.avif")';
+  static toggleImage = "./images/other/sun.avif";
 }
 
-class light{
+class light {
   static textColor = "black";
   static backgroundColor = "hsla(0, 0%, 95%, 0.3)";
   static windowBackgroundColor = "hsl(0, 0%, 95%)";
   static buttonBackgroundColor = "hsla(0, 0%, 65%, 0.5)";
-  static backgroundImage = 'url("images/backgrounds/light/mesh-776.avif")';
+  static backgroundImage = 'url("./images/backgrounds/light/mesh-776.avif")';
+  static toggleImage = "./images/other/moon.avif";
 }
 
 const darkTheme = {
@@ -35,53 +37,77 @@ init();
 
 themeToggle.forEach((element) => {
   element.addEventListener("click", function () {
-    for (let i = 0; i < themeToggle.length; i++) {
-      if (themeToggle[i].index % 2 == 0) {
-        for (let key in darkTheme) {
-          document.documentElement.style.setProperty(key, darkTheme[key]);
-        }
-        changeBackgroundImage(dark.backgroundImage);
-        themeToggle[i].children[0].src = "images/other/sun.avif";
-        themeToggle[i].index = 1;
-
-        // localStorage.setItem('themeColor', lightTheme["--text-color"]);
-        // let savedTheme = localStorage.getItem('themeColor');
-        // console.log(savedTheme);
-        // for(let key in savedTheme){
-        //   console.log(savedTheme[key]);
-        //   // document.documentElement.style.setProperty(key, savedTheme[key]);
-        // }
-      } else {
-        for (let key in lightTheme) {
-          document.documentElement.style.setProperty(key, lightTheme[key]);
-          // localStorage.setItem(key, lightTheme[key]);
-        }
-        changeBackgroundImage(light.backgroundImage);
-        themeToggle[i].children[0].src = "images/other/moon.avif";
-        themeToggle[i].index = 2;
-
-        // localStorage.setItem('themeColor', darkTheme);
-      }
-      // console.log(themeToggle[i].index);
+    if (element.index == 2) {
+      applyTheme("dark-theme");
+    } else {
+      applyTheme("light-theme");
     }
   });
 });
 
-function init(){
-  document.documentElement.style.setProperty("--background-color", dark.backgroundColor);
+function init() {
+  checkSavedTheme();
+
+  preloadImage("./images/backgrounds/dark/mesh-294.avif");
+  preloadImage("./images/backgrounds/light/mesh-776.avif");
+  preloadImage("./images/other/sun.avif");
+  preloadImage("./images/other/moon.avif");
+
   changeBackgroundImage(dark.backgroundImage);
 }
 
-function changeBackgroundImage(imgSrc){
+function applyTheme(theme) {
+  if (theme === "dark-theme") {
+    for (let i = 0; i < themeToggle.length; i++) {
+      themeToggle[i].index = 1;
+      changeToggleImage(i, dark.toggleImage);
+    }
+
+    for (let key in darkTheme) {
+      document.documentElement.style.setProperty(key, darkTheme[key]);
+    }
+
+    changeBackgroundImage(dark.backgroundImage);
+  } else if (theme === "light-theme") {
+    for (let i = 0; i < themeToggle.length; i++) {
+      themeToggle[i].index = 2;
+      changeToggleImage(i, light.toggleImage);
+    }
+
+    for (let key in lightTheme) {
+      document.documentElement.style.setProperty(key, lightTheme[key]);
+    }
+
+    changeBackgroundImage(light.backgroundImage);
+  }
+
+  localStorage.setItem("theme", theme);
+}
+
+function checkSavedTheme() {
+  document.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      applyTheme(savedTheme);
+    } else {
+      applyTheme("dark-theme");
+    }
+  });
+}
+
+function preloadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = url;
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(new Error(`Failed to load image at ${url}`));
+  });
+}
+
+function changeBackgroundImage(imgSrc) {
   body.style.backgroundImage = imgSrc;
 }
 
-// themeToggle.forEach((element) => {
-//   element
-
-// const savedTheme = localStorage.getItem('themeColor');
-// if (savedTheme != null){
-//   for(let key in savedTheme){
-//     document.documentElement.style.setProperty(key, savedTheme[key]);
-//   }
-// }
+function changeToggleImage(index, imgSrc) {
+  themeToggle[index].children[0].src = imgSrc;
+}
